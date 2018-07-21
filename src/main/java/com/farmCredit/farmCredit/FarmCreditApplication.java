5077f8dac608cdd0fe500186e4d54a1d1e9cd260
Ibijola <ibijola@cashenvoy.com>
2018-07-21 13:27:17 +0100

    initial commit

---
+++ src/main/java/com/farmCredit/farmCredit/FarmCreditApplication.java
@@ -0,0 +1,12 @@
+package com.farmCredit.farmCredit;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FarmCreditApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FarmCreditApplication.class, args);
+	}
+}
---
+++ src/test/java/com/farmCredit/farmCredit/FarmCreditApplicationTests.java
@@ -0,0 +1,16 @@
+package com.farmCredit.farmCredit;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class FarmCreditApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
